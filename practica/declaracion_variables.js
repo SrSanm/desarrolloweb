2a904aeb7639dcd5a1b87351e503478376b47ee8
SrSanm <asanmartinvalencia@gmail.com>
2026-02-21 13:16:49 -0500

    practica en casa

---
+++ practica/declaracion_variables.js
@@ -0,0 +1,11 @@
+// declaracion de variables antigua
+
+var nombre = "andres";
+
+// declaracion de variables moderno
+let nombre = "Andres"; //local
+const nombre = "ANdres";
+let n = 1;
+let n2 = 2.3;
+let diccion = [];
+let otro = {};
